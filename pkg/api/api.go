c05e213d924d2e1c3f2cb990fdbd7aaeba07fd71
Nattapong Amornbunchornvej <sprintf.null@gmail.com>
2019-01-20 20:28:02 +0700

    Record sending status when send mail

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -5,11 +5,16 @@ import (
 
 	"github.com/choestelus/try-mailer/pkg/mailer"
 	mex "github.com/choestelus/try-mailer/pkg/service/mailexporter"
+	"github.com/go-pg/pg"
 	"github.com/labstack/echo"
 	"github.com/sirupsen/logrus"
 )
 
-func MailerHandlerFunc(me *mex.MailExporter) func(c echo.Context) error {
+const (
+	RecordHistoryQuery = `INSERT INTO public.histories (email, access_from, status, failed) VALUES(?, ?, ?, ?)`
+)
+
+func MailerHandlerFunc(db *pg.DB, me *mex.MailExporter) func(c echo.Context) error {
 	return func(c echo.Context) error {
 		msg := mailer.Message{}
 
@@ -21,13 +26,34 @@ func MailerHandlerFunc(me *mex.MailExporter) func(c echo.Context) error {
 			})
 		}
 
+		allRecipients := append(msg.Recipients, msg.BCC, msg.CC)
+
 		err := me.SendMail(msg)
 		if err != nil {
 			logrus.Errorf("failed to send mail: [%v]", err)
+
+			// duplication over abstraction.
+			// inefficient, should not commit every insertion, but does the job.
+			for _, recp := range allRecipients {
+				_, err := db.ExecOne(RecordHistoryQuery, recp, c.RealIP(), err.Error(), true)
+				if err != nil {
+					logrus.Warnf("failed to record sending status to history: %v", err)
+				}
+			}
+
 			return c.JSON(http.StatusInternalServerError, map[string]interface{}{
 				"message": "failed to send mail",
 				"error":   err.Error(),
 			})
+		}
+
+		// duplication over abstraction.
+		// inefficient, should not commit every insertion, but does the job.
+		for _, recp := range allRecipients {
+			_, err := db.ExecOne(RecordHistoryQuery, recp, c.RealIP(), err.Error(), false)
+			if err != nil {
+				logrus.Warnf("failed to record sending status to history: %v", err)
+			}
 		}
 		return c.NoContent(http.StatusOK)
 	}
