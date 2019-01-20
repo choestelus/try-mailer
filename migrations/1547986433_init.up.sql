069e875aff1fedc317415aeefad98b8f15e029a7
Nattapong Amornbunchornvej <sprintf.null@gmail.com>
2019-01-20 20:27:36 +0700

    Add status and failed field

--- migrations/1547986433_init.up.sql
+++ migrations/1547986433_init.up.sql
@@ -6,6 +6,8 @@ CREATE TABLE public.histories (
     id                  uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     email               text NOT NULL,
     access_from         text,
+    status              text,
+    failed              bool NOT NULL,
     created_at          timestamp with time zone DEFAULT now(),
     updated_at          timestamp with time zone DEFAULT now()
 );
