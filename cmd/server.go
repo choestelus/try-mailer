bc0f3e00ffff4fce5025faa5a31fa1c31b768943
Nattapong Amornbunchornvej <sprintf.null@gmail.com>
2019-01-20 20:39:13 +0700

    Initialize DB connection

--- cmd/server.go
+++ cmd/server.go
@@ -3,11 +3,12 @@ package main
 import (
 	"github.com/choestelus/try-mailer/pkg/api"
 	mex "github.com/choestelus/try-mailer/pkg/service/mailexporter"
+	"github.com/go-pg/pg"
 	"github.com/labstack/echo"
 )
 
-func newServer(cfg Config, me *mex.MailExporter) *echo.Echo {
+func newServer(db *pg.DB, me *mex.MailExporter) *echo.Echo {
 	e := echo.New()
-	e.POST("/send", api.MailerHandlerFunc(me))
+	e.POST("/send", api.MailerHandlerFunc(db, me))
 	return e
 }
