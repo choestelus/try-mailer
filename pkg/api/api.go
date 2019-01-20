490efbb351f95b485c9fcce42b6059e05ede4f9e
Nattapong Amornbunchornvej <sprintf.null@gmail.com>
2019-01-20 18:43:22 +0700

    Fix error message not returning to http response

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -6,6 +6,7 @@ import (
 	"github.com/choestelus/try-mailer/pkg/mailer"
 	mex "github.com/choestelus/try-mailer/pkg/service/mailexporter"
 	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
 )
 
 func MailerHandlerFunc(me *mex.MailExporter) func(c echo.Context) error {
@@ -13,6 +14,7 @@ func MailerHandlerFunc(me *mex.MailExporter) func(c echo.Context) error {
 		msg := mailer.Message{}
 
 		if err := c.Bind(&msg); err != nil {
+			logrus.Errorf("failed to bind request: [%v]", err)
 			return c.JSON(http.StatusBadRequest, map[string]interface{}{
 				"message": "cannot bind request to internal message format",
 				"error":   err,
@@ -21,9 +23,10 @@ func MailerHandlerFunc(me *mex.MailExporter) func(c echo.Context) error {
 
 		err := me.SendMail(msg)
 		if err != nil {
+			logrus.Errorf("failed to send mail: [%v]", err)
 			return c.JSON(http.StatusInternalServerError, map[string]interface{}{
 				"message": "failed to send mail",
-				"error":   err,
+				"error":   err.Error(),
 			})
 		}
 		return c.NoContent(http.StatusOK)
