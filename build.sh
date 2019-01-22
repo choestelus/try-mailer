f6ea5320335fbc1127614177c0f5a783aef50faa
Nattapong Amornbunchornvej <sprintf.null@gmail.com>
2019-01-22 22:42:37 +0700

    Update build script to include ldflags

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 docker run --rm -it -v "$(pwd)":/go/src/github.com/choestelus/try-mailer \
     -w /go/src/github.com/choestelus/try-mailer golang:1.11-alpine sh -c 'echo "build..." && apk update && \
-    go build -o ./build/mailer ./cmd'
+    go build -ldflags "-X main.authKey=seup_key_here" -o ./build/mailer ./cmd'
